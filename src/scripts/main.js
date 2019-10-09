a2e6067ad4042c199f63a366079a4e52e48d4be9
Sullivan Pierce <sullypierce@gmail.com>
2019-10-09 14:41:59 -0500

    lightning exercises completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 console.log("Hello there.");
+
+//lightning exercises
+
+//exercise one
+
+const coolCar = {
+    "make" : "Ford",
+    model : "Mustang",
+    color : "red",
+    year : "2015"
+}
+console.log("coolCar", coolCar)
+
+//exercise two
+
+const shelterNames = ["Kippers","Jack","Gypsy","Angus","Seymour Boots","Sharky"];
+
+
+//exercise three
+
+const dad = {
+    age : 30,
+}
+
+const mom = {
+    age : 30
+}
+
+const sis = {
+    age :5
+}
+
+const bro = {
+    age : 2
+}
+
+const familyMembers = [dad,mom,sis,bro];
